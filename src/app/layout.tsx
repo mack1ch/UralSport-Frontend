0cfb0655f32b5fa6230fab931298eeff0ce23ecd
Dmitriy <dimastepanov.1995@mail.ru>
2023-12-03 16:10:06 +0500

    added: Metadata previewIMG

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next';
 import localFont from 'next/font/local';
 import './globals.scss';
 import Head from 'next/head';
-const openGraphImage = { images: ['../../public/assets/cover.png'] };
+
 const Mont = localFont({
     src: [
         {
@@ -26,14 +26,16 @@ const Mont = localFont({
 export const metadata: Metadata = {
     title: 'Урал Спорт',
     description: 'Админ панель для Министерства спорта Свердловской области',
-    ...openGraphImage,
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <>
             <Head>
-                <meta property="og:image" content="../../public/assets/cover.png" />
+                <meta
+                    property="og:image"
+                    content="https://www.jurn.io/content/images/2020/07/Figma-Thumbnails.png"
+                />
             </Head>
             <html lang="ru">
                 <body className={Mont.className}>{children}</body>
