04a84b8292a6b463e0d1956bec808d748c240760
Dmitriy <dimastepanov.1995@mail.ru>
2023-12-03 22:33:39 +0500

    added: questionnaire

---
+++ src/app/(auth)/questionnaire/page.tsx
@@ -0,0 +1,23 @@
+import Script from 'next/script';
+
+export default function Platform() {
+    return (
+        <>
+            <div
+                style={{
+                    width: '100vw',
+                    height: '100%',
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                }}>
+                <Script src="https://yastatic.net/s3/frontend/forms/_/embed.js"></Script>
+                <iframe
+                    src="https://forms.yandex.ru/cloud/656ca6d2c09c02a4b22dc234/?iframe=1"
+                    frameBorder="0"
+                    name="ya-form-656ca6d2c09c02a4b22dc234"
+                    width="650"></iframe>
+            </div>
+        </>
+    );
+}
